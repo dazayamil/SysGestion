b02d523fbc82e240bde31ba041204cbddbb78d26
Yamil Daza <dazayamil07@gmail.com>
2025-07-01 21:56:26 -0300

    feat: initial project

---
+++ src/main/java/com/techlab/sysgestion/SysGestionApplication.java
@@ -0,0 +1,13 @@
+package com.techlab.sysgestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysGestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techlab/sysgestion/SysGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techlab.sysgestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
