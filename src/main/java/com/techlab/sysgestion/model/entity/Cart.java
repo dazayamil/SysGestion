ff35b862e306c0da5761cd01716683cf727e77a6
Yamil Daza <dazayamil07@gmail.com>
2025-07-19 19:04:36 -0300

    refactor: restructure entity classes for clarity

--- src/main/java/com/techlab/sysgestion/model/entity/Cart.java
+++ src/main/java/com/techlab/sysgestion/model/entity/Cart.java
@@ -3,13 +3,15 @@ package com.techlab.sysgestion.model.entity;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
-@Data
+@Getter
 public class Cart {
     private double totalCost;
-    private List<CartItem> items;
+    private List<CartItem> items = new ArrayList<>();
 }
--- src/main/java/com/techlab/sysgestion/model/entity/Product.java
+++ src/main/java/com/techlab/sysgestion/model/entity/Product.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "product")
 @AllArgsConstructor
